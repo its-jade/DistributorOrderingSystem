a83c0f4d3db7058ae7f742f76ff16221b9d81f77
its-jade <108425675+its-jade@users.noreply.github.com>
2024-07-11 14:56:51 -0400

    Added comment to Main.java

--- src/main/java/com/example/distributororderingsystem/Main.java
+++ src/main/java/com/example/distributororderingsystem/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        // testing
     }
 
     public static void main(String[] args) {
