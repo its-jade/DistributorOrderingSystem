d3d35510c46512849ca8fb06b0c327ec84e5bc11
its-jade <108425675+its-jade@users.noreply.github.com>
2024-07-17 22:33:49 -0400

    Create responsive login (working)

--- src/main/java/com/example/distributororderingsystem/LoginController.java
+++ src/main/java/com/example/distributororderingsystem/LoginController.java
@@ -10,6 +10,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -25,7 +26,7 @@ public class LoginController implements Initializable {
     private TextField userNameField;
 
     @FXML
-    private TextField passwordField;
+    private PasswordField passwordField;
 
     @FXML
     private Button loginButton;
